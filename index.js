441474dd2b1007634e6b6341df6ae56fffdbf135
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2019-01-24 09:51:52 +0100

    I don't what I changed here

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /*jshint esversion: 6 */
-require('v8-compile-cache');
+/* eslint-disable no-console */
+require("v8-compile-cache");
 const multer = require("multer");
 const express = require("express");
 const rimraf = require("rimraf");
@@ -8,63 +9,63 @@ const path = require("path");
 const app = express();
 const port = 3000;
 const upload = multer({
-    dest: "./tmp/"
+	dest: "./tmp/"
 });
 const fs = require("fs");
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname + "/public/index.html"));
+	res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
 fs.access("./tmp", (err) => {
-    if (!err) {
-        rimraf("./tmp", function () {
-            fs.mkdir("./tmp", function () {
-                return;
-            });
-        });
-    }
-    //fs.mkdir("./tmp");
+	if (!err) {
+		rimraf("./tmp", function () {
+			fs.mkdir("./tmp", function () {
+				return;
+			});
+		});
+	}
+	//fs.mkdir("./tmp");
 });
 
 app.post("/file_upload", upload.single("image"), function (req, res) {
-    console.log(req.body);
-    console.log(req.file);
+	console.log(req.body);
+	console.log(req.file);
 
-    var file = __dirname + "/tmp/" + req.file.filename + path.extname(req.file.originalname);
+	var file = __dirname + "/tmp/" + req.file.filename + path.extname(req.file.originalname);
 
-    var fileObj = new Object({});
-    fileObj.name = req.file.originalname;
-    fileObj.filename = req.file.filename;
-    fileObj.size = req.file.size;
-    fileObj.mimetype = req.file.mimetype;
-    fileObj.password = req.body.password;
-    fileObj.deleteTime = req.body.deleteTime;
-    console.log("Deletes after: " + fileObj.deleteTime);
+	var fileObj = new Object({});
+	fileObj.name = req.file.originalname;
+	fileObj.filename = req.file.filename;
+	fileObj.size = req.file.size;
+	fileObj.mimetype = req.file.mimetype;
+	fileObj.password = req.body.password;
+	fileObj.deleteTime = req.body.deleteTime;
+	console.log("Deletes after: " + fileObj.deleteTime);
 
-    var fileObjJson = JSON.stringify(fileObj);
+	var fileObjJson = JSON.stringify(fileObj);
 
-    fs.writeFile(__dirname + "/tmp/" + req.file.filename + ".json", fileObjJson, function () {
-        return;
-    });
+	fs.writeFile(__dirname + "/tmp/" + req.file.filename + ".json", fileObjJson, function () {
+		return;
+	});
 
-    console.log(file);
-    fs.rename(req.file.path, file, function (err) {
-        if (err) {
-            res.sendStatus(500);
-            console.log(err);
-        } else {
-            temporaryHost(req.file.filename, path.extname(req.file.originalname), fileObj.password);
-        }
-    });
+	console.log(file);
+	fs.rename(req.file.path, file, function (err) {
+		if (err) {
+			res.sendStatus(500);
+			console.log(err);
+		} else {
+			temporaryHost(req.file.filename, path.extname(req.file.originalname), fileObj.password);
+		}
+	});
 });
 
 var temporaryHost = function (fileName, fileExtension, deleteTime) {
-    console.log("Hosting: " + String(fileName));
-    app.get("/files/" + String(fileName), function (req, res) {
-        res.sendFile(__dirname + "/tmp/" + String(fileName) + fileExtension);
-        res.send("<p>" + String(fileName) + "</p> <img src='" + "../tmp/" + String(fileName) + fileExtension + "'>");
-    });
+	console.log("Hosting: " + String(fileName));
+	app.get("/files/" + String(fileName), function (req, res) {
+		res.sendFile(__dirname + "/tmp/" + String(fileName) + fileExtension);
+		res.send("<p>" + String(fileName) + "</p> <img src='" + "../tmp/" + String(fileName) + fileExtension + "'>");
+	});
 };
 
 app.use(express.static("./public"));
--- public/upload/main.js
+++ public/upload/main.js
@@ -1,103 +1,104 @@
 /*jshint esversion: 6 */
+/* eslint-disable no-console */
 var fileToUpload;
 
 function sendImage() {
-    var formContent = new FormData();
-    formContent.append("image", fileToUpload);
-    formContent.append("name", "file");
+	var formContent = new FormData();
+	formContent.append("image", fileToUpload);
+	formContent.append("name", "file");
 
-    formContent.append("password", document.getElementById("upload-after-buttons-password").value);
+	formContent.append("password", document.getElementById("upload-after-buttons-password").value);
 
-    var selectedTime = 0;
-    var timeoutRange = document.getElementById("timeout-range").value;
-    if (timeoutRange == 1) {
-        selectedTime = 5;
-    } else {
-        selectedTime = timeoutRange * 5;
-    }
-    formContent.append("deleteTime", selectedTime);
+	var selectedTime = 0;
+	var timeoutRange = document.getElementById("timeout-range").value;
+	if (timeoutRange == 1) {
+		selectedTime = 5;
+	} else {
+		selectedTime = timeoutRange * 5;
+	}
+	formContent.append("deleteTime", selectedTime);
 
-    for (var pair of formContent.entries()) {
-        console.log(pair[0] + ', ' + pair[1]);
-    }
+	for (var pair of formContent.entries()) {
+		console.log(pair[0] + ', ' + pair[1]);
+	}
 
-    var request = new XMLHttpRequest();
-    request.open("POST", "/file_upload", true);
-    request.setRequestHeader("file", "image");
-    request.send(formContent);
+	var request = new XMLHttpRequest();
+	request.open("POST", "/file_upload", true);
+	request.setRequestHeader("file", "image");
+	request.send(formContent);
 }
 
 function fileUpload() {
-    document.getElementById("file-input").click();
+	document.getElementById("file-input").click();
 }
 
 function dropHandler(ev) {
-    ev.preventDefault();
+	ev.preventDefault();
 
-    if (ev.dataTransfer.items) {
-        for (i = 0; i < ev.dataTransfer.items.length; i++) {
-            if ((ev.dataTransfer.items[i].kind === "file") && (ev.dataTransfer.items[i].type.match("^image/*"))) {
-                //document.getElementById("test-image").style.backgroundImage = "url(" + URL.createObjectURL(ev.dataTransfer.files[i]) + ")";
-                document.getElementById("test-image-inside").src = URL.createObjectURL(ev.dataTransfer.files[i]);
-                //fileToUpload = URL.createObjectURL(ev.dataTransfer.files[i]);
-                fileToUpload = ev.dataTransfer.files[i];
-                uploadFinished();
-                console.log(fileToUpload);
-            }
-        }
-    }
-    removeDragData(ev);
+	if (ev.dataTransfer.items) {
+		for (i = 0; i < ev.dataTransfer.items.length; i++) {
+			if ((ev.dataTransfer.items[i].kind === "file") && (ev.dataTransfer.items[i].type.match("^image/*"))) {
+				//document.getElementById("test-image").style.backgroundImage = "url(" + URL.createObjectURL(ev.dataTransfer.files[i]) + ")";
+				document.getElementById("test-image-inside").src = URL.createObjectURL(ev.dataTransfer.files[i]);
+				//fileToUpload = URL.createObjectURL(ev.dataTransfer.files[i]);
+				fileToUpload = ev.dataTransfer.files[i];
+				uploadFinished();
+				console.log(fileToUpload);
+			}
+		}
+	}
+	removeDragData(ev);
 }
 
 function fileUploadButton(file) {
-    //document.getElementById("test-image").style.backgroundImage = "url(" + URL.createObjectURL(file[0]) + ")";
-    document.getElementById("test-image-inside").src = URL.createObjectURL(file[0]);
-    //fileToUpload = URL.createObjectURL(file[0]);
-    fileToUpload = file[0];
-    console.log(fileToUpload);
-    uploadFinished();
+	//document.getElementById("test-image").style.backgroundImage = "url(" + URL.createObjectURL(file[0]) + ")";
+	document.getElementById("test-image-inside").src = URL.createObjectURL(file[0]);
+	//fileToUpload = URL.createObjectURL(file[0]);
+	fileToUpload = file[0];
+	console.log(fileToUpload);
+	uploadFinished();
 }
 
 function dragOverHandler(ev) {
-    ev.preventDefault();
+	ev.preventDefault();
 }
 
 function removeDragData(ev) {
-    if (ev.dataTransfer.items) {
-        ev.dataTransfer.items.clear();
-    } else {
-        ev.dataTransfer.clearData();
-    }
+	if (ev.dataTransfer.items) {
+		ev.dataTransfer.items.clear();
+	} else {
+		ev.dataTransfer.clearData();
+	}
 }
 
 function uploadFinished() {
-    setTimeout(function () {
-        if (document.getElementById("test-image-inside").clientHeight <= 500) {
-            document.getElementById("upload-container").style.gridTemplateRows = document.getElementById("test-image-inside").clientHeight + 10 + "px";
-        } else {
-            document.getElementById("upload-container").style.gridTemplateRows = "500px";
-        }
-    }, 100);
-    document.getElementById("upload-div").style.display = "none";
-    document.getElementById("test-image").style.display = "block";
-    document.getElementById("upload-after-buttons").style.display = "block";
+	setTimeout(function () {
+		if (document.getElementById("test-image-inside").clientHeight <= 500) {
+			document.getElementById("upload-container").style.gridTemplateRows = document.getElementById("test-image-inside").clientHeight + 10 + "px";
+		} else {
+			document.getElementById("upload-container").style.gridTemplateRows = "500px";
+		}
+	}, 100);
+	document.getElementById("upload-div").style.display = "none";
+	document.getElementById("test-image").style.display = "block";
+	document.getElementById("upload-after-buttons").style.display = "block";
 }
 
 function updateDeleteTimer() {
-    var selectedTime = 0;
-    var timeoutRange = document.getElementById("timeout-range").value;
-    console.log(timeoutRange);
+	var selectedTime = 0;
+	var timeoutRange = document.getElementById("timeout-range").value;
+	console.log(timeoutRange);
 
-    if (timeoutRange == 1) {
-        selectedTime = 5;
-    } else {
-        selectedTime = timeoutRange * 5;
-    }
+	if (timeoutRange == 1) {
+		selectedTime = 5;
+	} else {
+		selectedTime = timeoutRange * 5;
+	}
 
-    document.getElementById("timeout-time-a").innerHTML = "Automaticly deletes after " + selectedTime + " minutes.";
+	document.getElementById("timeout-time-a").innerHTML = "Automaticly deletes after " + selectedTime + " minutes.";
 }
 updateDeleteTimer();
 
 function reloadPage() {
-    location.reload();
+	location.reload();
 }
